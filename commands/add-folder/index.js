883100250b6f7d59c88b8c7d39af59d38f801392
Pedro Martin <pataruco@gmail.com>
2018-06-19 13:04:12 +0100

    Abstract add-folder logic to a separate folder

---
+++ commands/add-folder/index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const isImage = require('is-image');
+const ExifImage = require('exif').ExifImage;
+const { DateTime } = require('luxon');
+
+const getDatePictureTaken = async image => {
+  try {
+    new ExifImage({ image }, async (e, exifData) => {
+      const datePictureTaken = await exifData.exif.DateTimeOriginal;
+      const dateArray = datePictureTaken.split(' ')[0].split(':');
+      const timeArray = datePictureTaken.split(' ')[1].split(':');
+
+      const date = await DateTime.local(
+        parseInt(dateArray[0], 10),
+        parseInt(dateArray[1], 10),
+        parseInt(dateArray[2], 10),
+        parseInt(timeArray[0], 10),
+        parseInt(timeArray[1], 10),
+        parseInt(timeArray[2], 10),
+      );
+      console.log(date);
+      return date;
+    });
+  } catch (e) {
+    console.error(error.message);
+  }
+};
+
+const getFiles = path => {
+  return fs.readdirSync(path).map(file => {
+    if (isImage(file)) {
+      return `${path}/${file}`;
+    }
+  });
+};
+
+const renameImagefilenames = async images => {
+  const dates = [];
+  images.forEach(image => {
+    try {
+      new ExifImage({ image }, async (_, exifData) => {
+        const datePictureTaken = await exifData.exif.DateTimeOriginal;
+        const dateArray = datePictureTaken.split(' ')[0].split(':');
+        const timeArray = datePictureTaken.split(' ')[1].split(':');
+
+        const date = await DateTime.local(
+          parseInt(dateArray[0], 10),
+          parseInt(dateArray[1], 10),
+          parseInt(dateArray[2], 10),
+          parseInt(timeArray[0], 10),
+          parseInt(timeArray[1], 10),
+          parseInt(timeArray[2], 10),
+        );
+        const dateString = `${date.year}-${date.month}-${date.day}`;
+        let counter = 0;
+
+        if (dates.includes(dateString)) {
+          // check why this is not adding
+          counter = counter + 1;
+          let newFileName = `${dateString}_${counter}`;
+          console.log(newFileName);
+          console.log(dates);
+        } else {
+          dates.push(dateString);
+        }
+      });
+    } catch (e) {
+      console.error(error.message);
+    }
+  });
+};
+
+const addFolder = async path => {
+  const images = await getFiles(path);
+
+  renameImagefilenames(images);
+};
+
+module.exports = { addFolder };
