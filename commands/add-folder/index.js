78f94d49f7f24c28ea390417516b7c6f1910c882
Pedro Martin <pataruco@gmail.com>
2018-06-19 20:11:20 +0100

    rename files by convention

--- commands/add-folder/index.js
+++ commands/add-folder/index.js
@@ -3,33 +3,6 @@ const isImage = require('is-image');
 const { ExifImage } = require('exif');
 const { DateTime } = require('luxon');
 
-// const getDatePictureTaken = async image => {
-//   try {
-//     new ExifImage({ image }, async (e, exifData) => {
-//       const datePictureTaken = await exifData.exif.DateTimeOriginal;
-//       const dateArray = datePictureTaken.split(' ')[0].split(':');
-//       const timeArray = datePictureTaken.split(' ')[1].split(':');
-
-//       const date = await DateTime.local(
-//         parseInt(dateArray[0], 10),
-//         parseInt(dateArray[1], 10),
-//         parseInt(dateArray[2], 10),
-//         parseInt(timeArray[0], 10),
-//         parseInt(timeArray[1], 10),
-//         parseInt(timeArray[2], 10),
-//       );
-//       console.log(date);
-//       return date;
-//     });
-//   } catch (e) {
-//     console.error(error.message);
-//   }
-// };
-
-// const getDatePictureTaken = image => {
-//   return new ExifImage({ image }, (e, exifData) => exifData);
-// };
-
 const getDatePictureTaken = image => {
   return new Promise((resolve, reject) => {
     try {
@@ -63,17 +36,40 @@ const getFiles = path => {
     .map(file => `${path}/${file}`);
 };
 
-const renameImagefilenames = async images => {
+let dates = [];
+let counter = 1;
+const getNewFileName = (path, date, fileExtension) => {
+  const dateString = `${date.year}-${date.month}-${date.day}`;
+
+  if (dates.includes(dateString)) {
+    counter++;
+    return `${path}/${dateString}_${counter}.${fileExtension}`;
+  } else {
+    counter = 1;
+    dates = [];
+    dates.push(dateString);
+    return `${path}/${dateString}_${counter}.${fileExtension}`;
+  }
+};
+
+const getfileExtension = fileString => {
+  const indexStart = fileString.length - 3;
+  return fileString.substring(indexStart);
+};
+
+const renameImagefilenames = async (path, images) => {
   for (const image of images) {
     const DateTimeOriginal = await getDatePictureTaken(image);
     const date = getDate(DateTimeOriginal);
-    console.log(date);
+    const fileExtension = getfileExtension(image);
+    newFileName = getNewFileName(path, date, fileExtension);
+    fs.renameSync(image, newFileName);
   }
 };
 
 const addFolder = path => {
   const images = getFiles(path);
-  renameImagefilenames(images);
+  renameImagefilenames(path, images);
 };
 
 module.exports = { addFolder };
