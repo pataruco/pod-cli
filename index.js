33827f3302fa62dfc8b269ce05df6f0b334f9de7
Pedro Martin <pataruco@gmail.com>
2018-06-20 10:58:53 +0100

    Update add-folder command description

--- index.js
+++ index.js
@@ -2,8 +2,11 @@
 const program = require('commander');
 const { addFolder } = require('./commands/add-folder');
 
-program.command('add-folder <dir>').action(dir => {
-  addFolder(dir);
-});
+program
+  .command('add-folder')
+  .description('Optimise images and upload it to S3')
+  .action(dir => {
+    addFolder(dir);
+  });
 
 program.parse(process.argv);
